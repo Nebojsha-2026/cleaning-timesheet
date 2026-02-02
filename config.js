e0bac936244f1ecf3c666534a6d9709df017d2d9
Nebojsha-2026 <work.petreski@gmail.com>
2026-02-02 23:08:19 +1100

    Update config.js

--- config.js
+++ config.js
@@ -5,4 +5,4 @@ window.CONFIG = window.CONFIG || {};
 
 // ✅ Paste your Supabase details between the quotes
 window.CONFIG.SUPABASE_URL = "https://hqmtigcjyqckqdzepcdu.supabase.co";
-window.CONFIG.SUPABASE_KEY = "YOUR_SUPABASE_ANON_KEY";
+window.CONFIG.SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhxbXRpZ2NqeXFja3FkemVwY2R1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjkwODgwMjYsImV4cCI6MjA4NDY2NDAyNn0.Rs6yv54hZyXzqqWQM4m-Z4g3gKqacBeDfHiMfpOuFRw";
